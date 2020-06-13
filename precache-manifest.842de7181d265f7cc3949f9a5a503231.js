f147cea473f61d008769675bfbaa8744a2ebc783
Jacob Godserv <jacobgodserv@gmail.com>
2020-06-13 00:25:06 -0400

    Updates

---
+++ precache-manifest.842de7181d265f7cc3949f9a5a503231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23087a2d0379ee2dd630da0fdfc99cfd",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "4941bc775a228431fe09",
+    "url": "/horizoverlay/static/css/main.4a5cd103.chunk.css"
+  },
+  {
+    "revision": "7a2f89d10b511b9c0d33",
+    "url": "/horizoverlay/static/js/2.b7868ab6.chunk.js"
+  },
+  {
+    "revision": "4941bc775a228431fe09",
+    "url": "/horizoverlay/static/js/main.4b820d90.chunk.js"
+  },
+  {
+    "revision": "f5cff2eb4effd23c272c",
+    "url": "/horizoverlay/static/js/runtime~main.5766f4f7.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
